4c1b90f57e8dccfb952a5aa21601140e1908f697
postsolarpunk <87605057+entzmingerc@users.noreply.github.com>
2024-01-16 21:16:54 -0600

    Upload mods to rudiments branch

--- rudiments.lua
+++ rudiments.lua
@@ -14,18 +14,22 @@
 -- ALT+K3 = randomize all voices
 
 engine.name = "Rudiments"
-
+nb = include("lib/nb/lib/nb")
+nb_voices = {"nb_1", "nb_2", "nb_3", "nb_4","nb_5", "nb_6", "nb_7", "nb_8"}
+track_notes = {60, 60, 60, 60, 60, 60, 60, 60}
+track_seq = {}
+track_idx = {}
+col_11_flag = 0
+for i = 1,8 do
+  track_seq[i] = {0, 0, 0, 0}
+  track_idx[i] = 1
+end 
 local last = 0
-
 local voice_count = 8
-
 local accents = 1
-
 local BeatClock = require 'beatclock'
-
 local clk = BeatClock.new()
 local all_midi = midi.connect()
-
 
 function setup_params()
   for i = 1,voice_count do
@@ -66,7 +70,14 @@ end
 
 function trigger(i)
   last = i
-  engine.trigger(i)
+  -- engine.trigger(i) -- triggers internal rudiments SC engine
+  local player = params:lookup_param(nb_voices[i]):get_player()
+  player:note_on(util.clamp(track_notes[i] + track_seq[i][track_idx[i]], 0, 127), 5)
+end
+
+function trigger_note_off(i)
+  local player = params:lookup_param(nb_voices[i]):get_player()
+  player:note_off(util.clamp(track_notes[i] + track_seq[i][track_idx[i]], 0, 127))
 end
 
 function randomize()
@@ -91,16 +102,20 @@ for i=1,8 do
 
   g:led(1,i,1)
   g:led(2,i,1)
-  g:led(3,i,3)
-  g:led(4,i,3)
+  g:led(3,i,6)
+  g:led(4,i,6)
   g:led(5,i,1)
   g:led(6,i,1)
-  g:led(7,i,3)
-  g:led(8,i,3)
-  g:led(9,i,5)
-  g:led(10,i,5)
-  
-  g:led(15,i,2)
+  g:led(7,i,6)
+  g:led(8,i,6)
+  g:led(9,i,1)
+  g:led(10,i,1)  
+  g:led(11,i,1)  
+  g:led(12,i,0)  
+  g:led(13,i,0)  
+  g:led(14,i,0)
+  g:led(15,i,0)    
+  g:led(16,i,2)
 
 end
 g:refresh()
@@ -139,34 +154,277 @@ g.key = function(x,y,z)
     elseif x == 5 then 
       -- OSC LOWER
       params:set("freq" .. y, util.clamp(params:get("freq" .. y)*0.9,20,10000))
+      track_notes[y] = util.clamp(track_notes[y] - 1, 0, 127)
+
     elseif x == 6 then 
       -- OSC HIGHER
       params:set("freq" .. y, util.clamp(params:get("freq" .. y)*1.1,20,10000))
+      track_notes[y] = util.clamp(track_notes[y] + 1, 0, 127)
       
     elseif x == 7 then
-      -- ENV DECAY
+      -- ENV DECAY LOWER
       params:set("decay" .. y, util.clamp(params:get("decay" .. y)*0.9,0.01,1))
       params:set("sweep" .. y, math.random(0,2000))
+
+      -- drumcrow = "amp_cycle"
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
+      if string.find(desc.name, "drumcrow") then
+        p_num = string.sub(desc.name, 10, 10)
+        p_name = "drumcrow_amp_cycle_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*1.15, 0.1, 500))
+
+      -- emplaitress = "decay"
+      elseif string.find(desc.name, "emplait") then
+        p_num = string.sub(desc.name, 9, 9)
+        p_name = "plaits_decay_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)-0.05, 0, 1))
+
+      -- nb_rudiments, "rudiments_decay"
+      elseif string.find(desc.name, "rudiments") then
+        p_num = string.sub(desc.name, 11, 11)
+        p_name = "rudiments_decay_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*0.9,0.05,1))
+
+      -- oilcan, "oilcan carrier release"
+      elseif string.find(desc.name, "Oilcan") then
+        p_num = string.sub(params:string(nb_voices[y]), 8, 8) -- uhhh oilcan name isn't numbered for some reason, get the string directly from nb param
+        for timbre = 1,7 do
+          p_name = "oilcan_car_rel_"..timbre.."_"..p_num
+          params:set(p_name, util.clamp(params:get(p_name)*0.9,0.01,3)) 
+        end
+
+      -- doubledecker, "doubledecker_amp_release_1" "doubledecker_amp_release_2"
+      elseif string.find(desc.name, "doubledecker") then
+        p_name = "doubledecker_amp_release_1"
+        params:set(p_name, util.clamp(params:get(p_name)*0.9, 0, 8))
+        p_name = "doubledecker_amp_release_2"
+        params:set(p_name, util.clamp(params:get(p_name)*0.9, 0, 8))
+
+      end
+
     elseif x == 8 then
-      -- ENV DECAY
+      -- ENV DECAY HIGHER
       params:set("decay" .. y, util.clamp(params:get("decay" .. y)*1.1,0.01,1))
       params:set("sweep" .. y, math.random(0,2000))
 
+      -- drumcrow = "amp_cycle"
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
+      if string.find(desc.name, "drumcrow") then
+        p_num = string.sub(desc.name, 10, 10)
+        p_name = "drumcrow_amp_cycle_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*0.85, 0, 500))
+
+      -- emplaitress = "decay"
+      elseif string.find(desc.name, "emplait") then
+        p_num = string.sub(desc.name, 9, 9)
+        p_name = "plaits_decay_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)+0.05, 0, 1))
+
+      -- nb_rudiments, "rudiments_decay"
+      elseif string.find(desc.name, "rudiments") then
+        p_num = string.sub(desc.name, 11, 11)
+        p_name = "rudiments_decay_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*1.1,0.05,1))
+
+      -- oilcan, "oilcan carrier release"
+      elseif string.find(desc.name, "Oilcan") then
+        p_num = string.sub(params:string(nb_voices[y]), 8, 8) -- uhhh oilcan name isn't numbered for some reason, get the string directly from nb param
+        for timbre = 1,7 do
+          p_name = "oilcan_car_rel_"..timbre.."_" .. p_num
+          params:set(p_name, util.clamp(params:get(p_name)*1.1,0.01,3)) 
+        end
+
+      -- doubledecker, "doubledecker_amp_release_1" "doubledecker_amp_release_2"
+      elseif string.find(desc.name, "doubledecker") then
+        p_name = "doubledecker_amp_release_1"
+        params:set(p_name, util.clamp(params:get(p_name)*1.1, 0, 8))
+        p_name = "doubledecker_amp_release_2"
+        params:set(p_name, util.clamp(params:get(p_name)*1.1, 0, 8))
+
+      end
       
     elseif x == 9 then
-      -- LFO FREQ
+      -- LFO FREQ LOWER
       params:set("lfoFreq" .. y, util.clamp(params:get("lfoFreq" .. y)*0.95,1,1000))
       -- params:set("lfoShape" .. y, math.random(0, 1))
       params:set("lfoSweep" .. y, math.random(0,2000))
+
+      -- drumcrow = "pw"
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
+      if string.find(desc.name, "drumcrow") then
+        p_num = string.sub(desc.name, 10, 10)
+        p_name = "drumcrow_pw_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)-0.05, -1, 1))
+
+      -- emplaitress = "harmonics"
+      elseif string.find(desc.name, "emplait") then
+        p_num = string.sub(desc.name, 9, 9)
+        p_name = "plaits_harmonics_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)-0.05, 0, 1))
+
+      -- nb_rudiments
+      elseif string.find(desc.name, "rudiments") then
+        p_num = string.sub(desc.name, 11, 11)
+        p_name = "rudiments_lfoFreq_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*0.9,1,1000)) 
+        p_name = "rudiments_lfoSweep_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*0.9,1,2000)) 
+      
+      -- oilcan, modulation release decrease
+      elseif string.find(desc.name, "Oilcan") then
+        p_num = string.sub(params:string(nb_voices[y]), 8, 8)
+        for timbre = 1,7 do
+          p_name = "oilcan_mod_rel_"..timbre.."_" .. p_num
+          params:set(p_name, util.clamp(params:get(p_name)*0.9,0.1,200)) 
+        end
+
+      -- doubledecker, "doubledecker_brilliance"
+      elseif string.find(desc.name, "doubledecker") then
+        p_name = "doubledecker_brilliance"
+        params:set(p_name, util.clamp(params:get(p_name)-0.05, -1, 1))
+
+
+      end
       
     elseif x == 10 then
-      -- LFO FREQ
+      -- LFO FREQ HIGHER
       params:set("lfoFreq" .. y, util.clamp(params:get("lfoFreq" .. y)*1.05,1,1000))
       -- params:set("lfoShape" .. y, math.random(0, 1))
       params:set("lfoSweep" .. y, math.random(0,2000))
 
+      -- drumcrow = "pw"
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
+      if string.find(desc.name, "drumcrow") then
+        p_num = string.sub(desc.name, 10, 10)
+        p_name = "drumcrow_pw_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)+0.05, -1, 1))
 
-    elseif x == 15 then
+      -- emplaitress = "harmonics"
+      elseif string.find(desc.name, "emplait") then
+        p_num = string.sub(desc.name, 9, 9)
+        p_name = "plaits_harmonics_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)+0.05, 0, 1))
+
+      -- nb_rudiments, increase both lfoFreq and lfoShape
+      elseif string.find(desc.name, "rudiments") then
+        p_num = string.sub(desc.name, 11, 11)
+        p_name = "rudiments_lfoFreq_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*1.1,1,1000))         
+        p_name = "rudiments_lfoSweep_"..p_num
+        params:set(p_name, util.clamp(params:get(p_name)*1.1,1,2000)) 
+
+      -- oilcan, modulation release increase
+      elseif string.find(desc.name, "Oilcan") then
+        p_num = string.sub(params:string(nb_voices[y]), 8, 8)
+        for timbre = 1,7 do
+          p_name = "oilcan_mod_rel_"..timbre.."_" .. p_num
+          params:set(p_name, util.clamp(params:get(p_name)*1.1,0.1,200)) 
+        end
+
+      -- doubledecker, "doubledecker_brilliance"
+      elseif string.find(desc.name, "doubledecker") then
+        p_name = "doubledecker_brilliance"
+        params:set(p_name, util.clamp(params:get(p_name)+0.05, -1, 1))
+
+      end
+
+    elseif x == 11 then
+      -- ON/OFF Switch, unique behavior for each nb voice
+
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
+      if col_11_flag == 0 then
+        -- drumcrow = "pw2" = random
+        if string.find(desc.name, "drumcrow") then
+          p_num = string.sub(desc.name, 10, 10)
+          params:set("drumcrow_bit_"..p_num, 1)
+          params:set("drumcrow_lfo_bit_"..p_num, 1.7)
+          params:set("drumcrow_amp_bit_"..p_num, 1)
+          
+        -- emplaitress = "fm_mod" = 1
+        elseif string.find(desc.name, "emplait") then
+          p_num = string.sub(desc.name, 9, 9)
+          params:set("plaits_fm_mod_"..p_num, 0.9)
+
+        -- nb_rudiments, set osc shape to 1
+        elseif string.find(desc.name, "rudiments") then
+          p_num = string.sub(desc.name, 11, 11) -- length of name + 2
+          params:set("rudiments_shape_" .. p_num, 1)      
+
+        -- oilcan, add 1 to note, midi note selects timbre on oilcan
+        elseif string.find(desc.name, "Oilcan") then
+          if track_notes[y] <= 126 and track_notes[y] >= 0 then -- max midi note 126 to add 1 to, min midi note is 0
+            track_notes[y] = track_notes[y] + 1
+          end
+
+        -- doubledecker, "pitch ratio 2" increment with wrapping
+        elseif string.find(desc.name, "doubledecker") then
+          p_name = "doubledecker_pitch_ratio_2"
+          params:set(p_name, util.wrap(params:get(p_name)+1, 1, 9))
+
+        end
+
+        col_11_flag = 1
+        g:led(x,y,15)
+        g:refresh()
+      else
+        -- drumcrow = "pw2" = 0
+        if string.find(desc.name, "drumcrow") then
+          p_num = string.sub(desc.name, 10, 10)
+          params:set("drumcrow_bit_"..p_num, 0)
+          params:set("drumcrow_lfo_bit_"..p_num, 0)
+          params:set("drumcrow_amp_bit_"..p_num, 0)
+
+        -- emplaitress = "fm_mod" = 0
+        elseif string.find(desc.name, "emplait") then
+          p_num = string.sub(desc.name, 9, 9)
+          params:set("plaits_fm_mod_"..p_num, 0)
+
+        -- nb_rudiments, set osc shape to 0
+        elseif string.find(desc.name, "rudiments") then
+          p_num = string.sub(desc.name, 11, 11) -- length of name + 2
+          params:set("rudiments_shape_" .. p_num, 0)
+
+        -- oilcan, subtract 1 from note, midi note selects timbre on oilcan
+        elseif string.find(desc.name, "Oilcan") then
+          if track_notes[y] <= 127 and track_notes[y] >= 1 then -- max midi note 127, min midi note to sub from is 1
+            track_notes[y] = track_notes[y] - 1
+          end
+
+        -- doubledecker, "pitch ratio 2" increment with wrapping
+        elseif string.find(desc.name, "doubledecker") then
+          p_name = "doubledecker_pitch_ratio_2"
+          params:set(p_name, util.wrap(params:get(p_name)+1, 1, 9))
+
+        end
+
+        col_11_flag = 0
+        g:led(x,y,1)
+        g:refresh()
+      end
+
+    elseif x == 12 or x == 13 or x == 14 or x == 15 then
+      x = x - 11
+      if track_seq[y][x] == 0 then
+        track_seq[y][x] = 5
+        g:led(x+11,y,1)
+      elseif track_seq[y][x] == 5 then
+        track_seq[y][x] = 7
+        g:led(x+11,y,5)
+      elseif track_seq[y][x] == 7 then
+        track_seq[y][x] = 12
+        g:led(x+11,y,9)
+      elseif track_seq[y][x] == 12 then
+        track_seq[y][x] = 0
+        g:led(x+11,y,0)
+      end
+      g:refresh()
+
+    elseif x == 16 then
       -- RANDOMIZE ALL
       params:set("shape" .. y, math.random(0, 1))
       params:set("freq" .. y, math.random(20, 10000))
@@ -175,10 +433,85 @@ g.key = function(x,y,z)
       params:set("lfoFreq" .. y, math.random(1, 1000))
       params:set("lfoShape" .. y, math.random(0, 1))
       params:set("lfoSweep" .. y, math.random(0, 2000))
-      
+
+      local player = params:lookup_param(nb_voices[y]):get_player()
+      desc = player:describe()
 
 
 
+      -- drumcrow
+      if string.find(desc.name, "drumcrow") then
+        p_num = string.sub(desc.name, 10, 10) -- length of name + 2
+        params:set("drumcrow_amp_cycle_"..p_num, math.random() * 35 + 0.1)
+        params:set("drumcrow_pw_"..p_num, math.random() * 2 - 1)
+        params:set("drumcrow_pw2_"..p_num, math.random() * 20 - 10)
+        -- randomize nb midi note
+        track_notes[y] = math.floor(math.random()*80+20)
+
+      -- emplaitress
+      elseif string.find(desc.name, "emplait") then
+        p_num = string.sub(desc.name, 9, 9) -- length of name + 2
+        params:set("plaits_decay_"..p_num, math.random()*0.8)
+        params:set("plaits_harmonics_"..p_num, math.random()*0.8)
+        params:set("plaits_timbre_"..p_num, math.random()*0.8)
+        params:set("plaits_morph_"..p_num, math.random()*0.8)
+        -- randomize nb midi note
+        track_notes[y] = math.floor(math.random()*80+20)
+
+      -- nb_rudiments
+      elseif string.find(desc.name, "rudiments") then
+        p_num = string.sub(desc.name, 11, 11) -- length of name + 2
+        -- everything except osc shape, set that with col 11
+        params:set("rudiments_freq_" .. p_num, math.random(20, 10000))
+        params:set("rudiments_decay_" .. p_num, math.random())
+        params:set("rudiments_sweep_" .. p_num, math.random(0, 2000))
+        params:set("rudiments_lfoFreq_" .. p_num, math.random(1, 1000))
+        params:set("rudiments_lfoShape_" .. p_num, math.random(0, 1))
+        params:set("rudiments_lfoSweep_" .. p_num, math.random(0, 2000))
+        -- randomize nb midi note
+        track_notes[y] = math.floor(math.random()*80+20)
+
+      -- oilcan      
+      elseif string.find(desc.name, "Oilcan") then
+        p_num = string.sub(params:string(nb_voices[y]), 8, 8)
+        -- each player of oilcan has 7 timbres
+        -- "oilcan_mod_ratio_1_3" example of parameter name where 1 is timbre (1-7) 3 is player (1-4?)
+        -- I think randomize all timbres sounds like a plan
+        for timbre = 1,7 do
+          params:set("oilcan_freq_"..timbre.."_" .. p_num, 600*math.random()^2 + 5) -- freq
+          params:set("oilcan_sweep_time_"..timbre.."_" .. p_num, math.random()*2.5) -- sweep time
+          params:set("oilcan_sweep_ix_"..timbre.."_" .. p_num, math.random()*0.6 - 0.3) -- sweep index
+          params:set("oilcan_car_rel_"..timbre.."_" .. p_num, math.random()) -- carrier release
+          params:set("oilcan_mod_rel_"..timbre.."_" .. p_num, math.random()*100) -- modulator release
+          params:set("oilcan_mod_ix_"..timbre.."_" .. p_num, math.random()*0.25) -- modulator level
+          params:set("oilcan_mod_ratio_"..timbre.."_" .. p_num, 10*math.random()^2) -- modulator ratio
+          params:set("oilcan_fb_"..timbre.."_" .. p_num, math.random()*1.5) -- feedback
+          params:set("oilcan_fold_"..timbre.."_" .. p_num, math.random()^2*15) -- fold
+        end
+
+      -- doubledecker
+      elseif string.find(desc.name, "doubledecker") then
+        params:set("doubledecker_brilliance", math.random()*2 - 1)
+        params:set("doubledecker_amp_release_1", math.random()*3)
+        params:set("doubledecker_amp_release_2", math.random()*3)
+        params:set("doubledecker_portomento", math.random()*0.25)
+        params:set("doubledecker_lp_freq_1", math.random()*1500 + 100)
+        params:set("doubledecker_lp_freq_2", math.random()*1500 + 100)
+        params:set("doubledecker_lp_res_1", math.random()*0.9)
+        params:set("doubledecker_lp_res_2", math.random()*0.9)
+        params:set("doubledecker_hp_freq_1", math.random()*1500 + 20)
+        params:set("doubledecker_hp_freq_2", math.random()*1500 + 20)
+        params:set("doubledecker_hp_res_1", math.random()*0.9)
+        params:set("doubledecker_hp_res_2", math.random()*0.9)
+        params:set("doubledecker_filter_init_1", math.random()*2-1)
+        params:set("doubledecker_filter_init_2", math.random()*2-1)
+        params:set("doubledecker_filter_attack_level_1", math.random()*2-1)
+        params:set("doubledecker_filter_attack_level_2", math.random()*2-1)
+
+
+      end
+      g:led(x,y,math.random(1,15))
+      g:refresh()
     end
   end
 end
@@ -231,16 +564,45 @@ local function trig()
       if accents==1 then
         params:set("lfoShape" .. i, math.random(0, 1))
       end
+      track_idx[i] = track_idx[i] % 4 + 1
       trigger(i)
       g:led(1,i,15)
+
+      -- update sequencer LEDs
+      for j = 1,4 do
+        if track_seq[i][j] == 0 then
+          g:led(j+11,i,0)
+        elseif track_seq[i][j] == 5 then
+          g:led(j+11,i,1)
+        elseif track_seq[i][j] == 7 then
+          g:led(j+11,i,5)
+        elseif track_seq[i][j] == 12 then
+          g:led(j+11,i,9)
+        end
+      end
+      g:led(track_idx[i]+11,i,15)
     else
+      trigger_note_off(i)
       g:led(1,i,1)
     end
+    g:led(16,i,math.random(1,15))
     g:refresh()
   end
 end
 
 function init()
+  nb.voice_count = 1
+  nb:init()
+  nb:add_param(nb_voices[1], nb_voices[1])
+  nb:add_param(nb_voices[2], nb_voices[2])
+  nb:add_param(nb_voices[3], nb_voices[3])
+  nb:add_param(nb_voices[4], nb_voices[4])
+  nb:add_param(nb_voices[5], nb_voices[5])
+  nb:add_param(nb_voices[6], nb_voices[6])
+  nb:add_param(nb_voices[7], nb_voices[7])
+  nb:add_param(nb_voices[8], nb_voices[8])
+  nb:add_player_params()
+
   params:add_separator()
   clk:add_clock_params()
   setup_params()
@@ -254,10 +616,9 @@ function init()
   clk.on_step = step
   clk.on_select_internal = function() clk:start() end
   clk.on_select_external = reset_pattern
-
   params:default()
-
   clk:start()
+
 end
 
 function reset_pattern()
